910a60c4dba6248be5fe5c2496d9a283b998e39d
Steve Mustafa <stemusta@amazon.com>
2021-01-05 14:15:46 +0200

    updated the template file - removing all the old code

--- __init__.py
+++ __init__.py
@@ -3,6 +3,3 @@ from . import functions
 from .functions import file_validator, notify_invalid
 from .tests import integration, unit
 
-## old files
-from .functions import stock_seller, stock_buyer, stock_checker
-
